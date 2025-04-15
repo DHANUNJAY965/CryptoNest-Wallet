95c93e40afacf704f91d4970e1d8a424e867eea5
DHANUNJAY965 <dhanunjayaburada6@gmail.com>
2025-04-15 11:31:13 +0530

    added the alchemy mainnet endpoints

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,8 @@
 export const NETWORK_ENDPOINTS = {
-  ETHEREUM: "https://eth-sepolia.g.alchemy.com/v2/demo",
-  SOLANA: "https://api.devnet.solana.com"
+  // ETHEREUM: "https://eth-sepolia.g.alchemy.com/v2/demo",
+  ETHEREUM: "https://eth-mainnet.g.alchemy.com/v2/AkXSZiztwKp55SZWaBjvujq0qBuMM6bz",
+  // SOLANA: "https://api.mainnet-beta.solana.com"
+  SOLANA: "https://solana-mainnet.g.alchemy.com/v2/AkXSZiztwKp55SZWaBjvujq0qBuMM6bz"
 } as const;
 
 export const STORAGE_KEYS = {
