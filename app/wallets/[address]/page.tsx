5f7bbd9939867a94bc669b41957942d9252116a1
DHANUNJAY965 <dhanunjayaburada6@gmail.com>
2025-06-01 18:01:14 +0530

    resolved the build error due to delete file

--- app/wallets/[address]/page.tsx
+++ app/wallets/[address]/page.tsx
@@ -11,7 +11,6 @@ import {
   Loader2
 } from "lucide-react";
 import { Wallet, WalletBalance } from "@/lib/types";
-import { getWalletBalance } from "@/lib/wallet";
 import { STORAGE_KEYS } from "@/lib/constants";
 import { toast } from "sonner";
 
